a7839a4fb369d4ee062aeac5e4e8cd614744b11c
zachvalenta <zjv5002@gmail.com>
2019-03-12 20:16:31 -0400

    switch test impl to pytest

--- test/test_warmup.py
+++ test/test_warmup.py
@@ -1,57 +1,47 @@
-import unittest
 from src.warmup import counting_valleys, jump_clouds, repeated_string,\
     sock_merchant
 
 
-class TestApp(unittest.TestCase):
+def test_counting_valleys():
+    """
+    _/\_______
+       \    /
+        \/\/
+    """
+    assert counting_valleys(s='UDDDUDUU') == 1
+    """
+    __________
+    \/\      /
+       \  /\/
+        \/
+    """
+    assert counting_valleys(s='DDUUDDUDUUUD') == 2
+    """
+         /\/\
+        /    \
+    _/\/______\
+    """
+    assert counting_valleys(s='UDUUUDUDDD') == 0
+    """
+    __________________
+    \/\      /
+       \  /\/
+        \/
+    """
+    assert counting_valleys(s='DUDDDUUDUU') == 2
 
-    def test_counting_valleys(self):
-        """
-        _/\_______
-           \    /
-            \/\/
-        """
-        valleys1 = 'UDDDUDUU'
-        self.assertEqual(counting_valleys(s=valleys1), 1)
 
-        """
-        __________
-        \/\      /
-           \  /\/
-            \/
-        """
-        valleys2 = 'DDUUDDUDUUUD'
-        self.assertEqual(counting_valleys(s=valleys2), 2)
+def test_jump_clouds():
+    assert jump_clouds([0, 0, 0, 1, 0, 0]) == 3
+    assert jump_clouds([0, 1, 0, 0, 0, 1, 0]) == 3
+    assert jump_clouds([0, 0, 1, 0, 0, 1, 0]) == 4
 
-        """
-        UDUUUDUDDD
 
-             /\/\
-            /    \
-        _/\/______\
-        """
-        valleys3 = 'UDUUUDUDDD'
-        self.assertEqual(counting_valleys(s=valleys3), 0)
+def test_repeat_string():
+    assert repeated_string(s='aba', n=10) == 7
+    assert repeated_string(s='a', n=1000000000000) == 1000000000000
+    assert repeated_string(s='aab', n=882787) == 588525
 
-        """
-        __________________
-        \/\      /
-           \  /\/
-            \/
-        """
-        valleys4 = 'DUDDDUUDUU'
-        self.assertEqual(counting_valleys(s=valleys4), 2)
 
-    def test_jump_clouds(self):
-        self.assertEqual(jump_clouds([0, 0, 0, 1, 0, 0]), 3)
-        self.assertEqual(jump_clouds([0, 1, 0, 0, 0, 1, 0]), 3)
-        self.assertEqual(jump_clouds([0, 0, 1, 0, 0, 1, 0]), 4)
-
-    def test_repeated_string(self):
-        self.assertEqual(repeated_string(s='aba', n=10), 7)
-        self.assertEqual(repeated_string(s='a', n=1000000000000), 1000000000000)
-        self.assertEqual(repeated_string(s='aab', n=882787), 588525)
-
-    def test_sock_merchant(self):
-        potential_pairs = [10, 20, 20, 10, 10, 30, 50, 10, 20]
-        self.assertEqual(sock_merchant(ar=potential_pairs), 3)
+def test_sock_merchant():
+    assert sock_merchant(ar=[10, 20, 20, 10, 10, 30, 50, 10, 20]) == 3
