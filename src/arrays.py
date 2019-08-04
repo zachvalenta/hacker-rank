1698b6ca97cf59a26b50af9f0c3a4b53f8e1ad62
zachvalenta <zjv5002@gmail.com>
2019-08-04 19:26:11 -0400

    arrays - 2d - first stab, few test cases remain

---
+++ src/arrays.py
@@ -0,0 +1,16 @@
+"""
+https://www.hackerrank.com/interview/interview-preparation-kit/arrays/challenges
+"""
+
+def sum_hourglass(arr):
+    global_high = 0
+    for ind, two_d in list(enumerate(arr))[:4]:
+        for sy, el in list(enumerate(two_d))[:4]:
+            local_high = 0
+            top_sum = sum(two_d[sy:sy+3])
+            mid_sum = arr[ind+1][sy+1]
+            btm_sum = sum(arr[ind+2][sy:sy+3])
+            local_high += top_sum + mid_sum + btm_sum
+            if local_high >= global_high:
+                global_high = local_high
+    return global_high
