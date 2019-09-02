8d2e32f497ab0aa44f919115f431488ed88532f9
zachvalenta <zjv5002@gmail.com>
2019-09-02 18:16:41 -0400

    maps - common substring (first try for once :) )

---
+++ src/maps.py
@@ -0,0 +1,5 @@
+def common_substring(s1,s2):
+    for x in s1:
+        if s2.find(x) > -1:
+            return 'YES'
+    return 'NO'
---
+++ test/test_maps.py
@@ -0,0 +1,5 @@
+from src.maps import common_substring
+
+def test_common_substring():
+    assert common_substring('hello', 'world') == 'YES'
+    assert common_substring('hi', 'world') == 'NO'
